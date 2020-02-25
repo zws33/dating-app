e61b7699732b3d882cf073087c3114521192a7be
Zachary Smith <zach.smith33@gmail.com>
2020-02-25 18:30:21 -0500

    set up fragment and viewmodel to render fake data.

--- app/src/main/java/me/zwsmith/datingapp/ui/matches/MatchesFragment.kt
+++ app/src/main/java/me/zwsmith/datingapp/ui/matches/MatchesFragment.kt
@@ -1,12 +1,18 @@
 package me.zwsmith.datingapp.ui.matches
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import me.zwsmith.datingapp.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -17,9 +23,12 @@ class MatchesFragment : Fragment() {
 
     private val matchesViewModel: MatchesViewModel by viewModel()
 
+    private val matchesListAdapter: MatchListAdapter by lazy { MatchListAdapter(requireContext()) }
+    private lateinit var matchesList: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        matchesViewModel.setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        matchesViewModel.loadMatches()
     }
 
     override fun onCreateView(
@@ -27,24 +36,22 @@ class MatchesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        matchesViewModel.text.observe(viewLifecycleOwner, Observer<String> {
-            textView.text = it
-        })
+        matchesList = root.findViewById(R.id.matches_rv)
+        matchesList.adapter = matchesListAdapter
+        matchesList.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        matchesViewModel.viewStates.observe(viewLifecycleOwner, Observer { data ->
+            matchesListAdapter.matchesData = data
+        })
+    }
+
     companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
         private const val ARG_SECTION_NUMBER = "section_number"
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         @JvmStatic
         fun newInstance(sectionNumber: Int): MatchesFragment {
             return MatchesFragment().apply {
@@ -54,4 +61,50 @@ class MatchesFragment : Fragment() {
             }
         }
     }
+}
+
+class MatchListAdapter(
+    private val context: Context
+): RecyclerView.Adapter<MatchListAdapter.MatchItem>() {
+    var matchesData: List<MatchItemViewState> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class MatchItem(view: View): RecyclerView.ViewHolder(view) {
+        val username: TextView = view.findViewById(R.id.username_tv)
+        val age: TextView = view.findViewById(R.id.age_tv)
+        val location: TextView = view.findViewById(R.id.location_tv)
+        val matchPercent: TextView = view.findViewById(R.id.match_percent_tv)
+        val profilePhoto: AppCompatImageView = view.findViewById(R.id.profile_photo_iv)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchItem {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.match_card, parent, false)
+        return MatchItem(view)
+    }
+
+    override fun getItemCount(): Int {
+        return matchesData.size
+    }
+
+    override fun onBindViewHolder(holder: MatchItem, position: Int) {
+        val viewState: MatchItemViewState = matchesData[position]
+        with(holder) {
+            username.text = viewState.username
+            age.text = viewState.age
+            location.text = getLocationString(viewState)
+            matchPercent.text = context.getString(R.string.match_percent, viewState.matchPercent)
+            val picasso = Picasso.get().apply {
+                isLoggingEnabled = true
+            }
+            picasso
+                .load(viewState.imageUrl)
+                .into(profilePhoto)
+        }
+    }
+
+    private fun getLocationString(viewState: MatchItemViewState) =
+        context.getString(R.string.location, viewState.city, viewState.stateCode)
 }
--- app/src/main/java/me/zwsmith/datingapp/ui/matches/MatchesViewModel.kt
+++ app/src/main/java/me/zwsmith/datingapp/ui/matches/MatchesViewModel.kt
@@ -2,18 +2,33 @@ package me.zwsmith.datingapp.ui.matches
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import me.zwsmith.datingapp.domain.Repository
 
 class MatchesViewModel(private val matchesRepository: Repository) : ViewModel() {
 
-    private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
-    }
+    val viewStates: LiveData<List<MatchItemViewState>> get() = _viewStates
+    private val _viewStates = MutableLiveData<List<MatchItemViewState>>()
 
-    fun setIndex(index: Int) {
-        _index.value = index
+    fun loadMatches() {
+        _viewStates.value = (0..5).map {
+            MatchItemViewState(
+                username = "asianbluedust",
+                age = "29",
+                city = "Teaneck",
+                stateCode = "NJ",
+                matchPercent = "47",
+                imageUrl = "https://i.pinimg.com/originals/24/6d/5d/246d5dbabe52ebd7ce807f2f293ee892.jpg"
+            )
+        }
     }
 }
+
+data class MatchItemViewState(
+    val username: String,
+    val age: String,
+    val city: String,
+    val stateCode: String,
+    val matchPercent: String,
+    val imageUrl: String
+)
