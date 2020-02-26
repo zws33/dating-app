8f2c98294aa24f611e94825ae1d54576cc95884c
Zachary Smith <zach.smith33@gmail.com>
2020-02-25 22:11:24 -0500

    Update appbar and statusbar

--- app/src/main/java/me/zwsmith/datingapp/MainActivity.kt
+++ app/src/main/java/me/zwsmith/datingapp/MainActivity.kt
@@ -2,6 +2,7 @@ package me.zwsmith.datingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import me.zwsmith.datingapp.ui.matches.MatchesPagerAdapter
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
         val sectionsPagerAdapter = MatchesPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
